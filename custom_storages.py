3628bffa7078e217332af706ecfef0e61667134d
Ali Khundmiri <salikhundmiri@gmail.com>
2020-03-22 09:36:47 +0530

    upgraded from S3BotoStorage to S3Boto3Storage

--- custom_storages.py
+++ custom_storages.py
@@ -1,13 +1,14 @@
 
 # custom_storages.py
 from django.conf import settings
-from storages.backends.s3boto import S3BotoStorage
+# from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 # In order for our STATICFILES_STORAGE to have different settings from our DEFAULT_FILE_STORAGE, they need to use two different storage classes
 # this file will help create a new filder with different settings in our s3 bucket
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(S3Boto3Storage):
     location = settings.CUSTOM_PROJECT_NAME +"/"+ settings.STATICFILES_LOCATION
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     location = settings.CUSTOM_PROJECT_NAME +"/"+ settings.MEDIAFILES_LOCATION
