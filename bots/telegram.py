21400242de1c0161d817d4182960b43a24d8ce39
Ali Khundmiri <salikhundmiri@gmail.com>
2020-05-31 12:30:33 +0530

    working with send pair key issue

--- bots/telegram.py
+++ bots/telegram.py
@@ -15,6 +15,7 @@ For a description of the Bot API, see this page: https://core.telegram.org/bots/
 '''
 
 import os
+import json
 import requests
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 print(TELEGRAM_TOKEN)
@@ -52,7 +53,7 @@ if __name__ == '__main__':
 
 def send_pair_url(link, chat_id):
 	url  = 'https://api.telegram.org/bot{}/sendMessage'.format(TELEGRAM_TOKEN)
-	payload = {"chat_id":chat_id, "text":"📎 Here's your pair link", reply_markup: {"inline_keyboard": [[{"text":"🔑 Pair", "url": link,}]]}}
+	payload = {"chat_id":chat_id, "text":"📎 Here's your pair link", reply_markup: json.dumps({"inline_keyboard": [[{"text":"🔑 Pair", "url": link,}]]}) }
 	print("url ", url)
 	print("payload ", payload)
 	r = requests.post(url, data=payload)
